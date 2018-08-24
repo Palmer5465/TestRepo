2477fdbc602fcdef2dc6bb8bf4ef153575084262
Jason <jasonpalmer17@augustana.edu>
2018-08-24 13:45:05 -0500

    Added the new Eclipse project to the repo

---
+++ TestProject/src/TestMain.java
@@ -0,0 +1,15 @@
+/*
+ * Name: Jason Palmer
+ * Date: 8/24/18
+ * Course: CSC 285
+ * Program: TestProject
+ * 
+ * Description: This is a test project
+ */
+
+public class TestMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Chicken World!");
+	}
+}
